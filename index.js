bbb6947a92dc81f0bc8bafd31a45b13c6109ea81
Leshan <leshanpasindu12@gmail.com>
2025-11-15 09:05:06 +0530

    get cli input after that create a content

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
-console.log('hello world');
+const note = process.argv[2]; // Get the note from command line arguments
+
+const newNote = {
+    content: note,
+    id : Date.now() // Unique ID based on timestamp
+}
+
+console.log("Note added:", newNote);
