b3d94200b7f8379baed9bcd9640f0468b815256f
Leshan <leshanpasindu12@gmail.com>
2025-11-15 09:16:21 +0530

    how to create own module and import it  where you want

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
 
-const note = process.argv[2]; // Get the note from command line arguments
+import {count} from './utils'
 
+const note = process.argv[2]; // Get the note from command line arguments
 const newNote = {
     content: note,
     id : Date.now() // Unique ID based on timestamp
 }
-
-console.log("Note added:", newNote);
---
+++ utils.js
@@ -0,0 +1,1 @@
+export const count = number => number + 1
