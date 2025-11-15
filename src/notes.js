251828fc444476899fd187a7a6f6c6a7bd9ad2a7
Leshan <leshanpasindu12@gmail.com>
2025-11-15 20:13:16 +0530

    create findNotes function

--- src/notes.js
+++ src/notes.js
@@ -12,3 +12,15 @@ export const newNote = async (note, tags) => {
   await insetDB(newNote);
   return newNote;
 }
+
+// Function to get all notes
+export const getAllNotes = async () => {
+  const {notes} = await getDB();
+  return notes;
+}
+
+// Function to find notes by filter
+export const findNotes = async (filter) => {
+  const { notes } = await getDB();
+  return notes.filter( note => note.content.toLowerCase().includes(filter.toLowerCase())); // find notes using given keywords
+}
